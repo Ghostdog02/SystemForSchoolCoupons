14e03e40fe46461fadf8aa4a80e4e46c73bc36d4
Ghostdog02 <alex.vesely07@gmail.com>
2024-08-15 17:26:30 +0300

    Added Seeding Method witch adds on every instance roles and Admin User

--- Coupons.Database/CouponsContext.cs
+++ Coupons.Database/CouponsContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Coupons.Database
 {
@@ -45,6 +47,81 @@ namespace Coupons.Database
             {
                 entity.ToTable("UserTokens");
             });
+        }
+    }
+
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            //var context = serviceProvider.GetRequiredService<CouponsContext>();
+            var context = scope.ServiceProvider.GetRequiredService<CouponsContext>();
+
+            string[] roles = ["Admin", "Parent", "Cook"];
+
+            foreach (string role in roles)
+            {
+                var roleStore = new CustomRoleStore(context);
+
+                if (!context.Roles.Any(r => r.Name == role))
+                {
+                    roleStore.CreateAsync(new IdentityRole<int>(role));
+                }
+            }
+
+
+            var user = new User
+            {
+                UserName = "Admin",
+                Email = "adming@admin.com",
+                NormalizedEmail = "ADMIN@ADMIN.COM",
+                NormalizedUserName = "OWNER",
+                PhoneNumber = "0882452245",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString("D")
+            };
+
+
+            if (!context.Users.Any(u => u.UserName == user.UserName))
+            {
+                var password = new PasswordHasher<User>();
+                var hashed = password.HashPassword(user, "secret");
+                user.PasswordHash = hashed;
+
+                var userStore = new CustomUserStore(context);
+                var result = userStore.CreateAsync(user);
+
+            }
+
+            AssignRoles(serviceProvider, user.Email, roles);
+
+            context.SaveChangesAsync();
+        }
+
+        public static async Task<IdentityResult> AssignRoles(IServiceProvider services, string email, string[] roles)
+        {
+            UserManager<User> _userManager = services.GetService<UserManager<User>>();
+            User user = await _userManager.FindByEmailAsync(email);
+            var result = await _userManager.AddToRolesAsync(user, roles);
+
+            return result;
+        }
+
+    }
+
+    public class CustomRoleStore : RoleStore<IdentityRole<int>, CouponsContext, int>
+    {
+        public CustomRoleStore(CouponsContext context) : base(context)
+        {
+        }
+    }
+
+    public class CustomUserStore : UserStore<User, IdentityRole<int>, CouponsContext, int>
+    {
+        public CustomUserStore(CouponsContext context) : base(context)
+        {
         }
     }
 }
