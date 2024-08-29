d26c50682b1951a6d1ff7c65dc563882be4132d9
Ghostdog02 <alex.vesely07@gmail.com>
2024-08-29 14:36:09 +0300

    Added Parent and Admin Controllers with the given Index Actions

---
+++ SchoolCoupons/Controllers/ParentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolCoupons.Controllers
+{
+    [Authorize(Roles = "Admin,Parent")]
+    public class ParentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
